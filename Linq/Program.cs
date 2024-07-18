402d7046a2973ce698022951d435231afbe35f6a
AntonijoTomic <antonijo.tomic12@gmail.com>
2024-07-18 18:01:19 +0200

    Vkjezba

--- Linq/Program.cs
+++ Linq/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
 
 namespace Linq
 {
@@ -8,78 +11,146 @@ namespace Linq
     {
         static void Main(string[] args)
         {
-            //PrviSat();
+             //Prvi();
 
+            //Drugi();
+
+
+           peti();
             Console.ReadLine();
-
         }
 
-        private static void PrviSat()
+        private static void peti()
         {
-            string[] gradovi = { "Varazdin", "Zagreb", "Osijek", "Sisak", "Sibenik" };
+            Skladiste n1 = new Skladiste(1, "Zagreb", 300);
+            Skladiste n2 = new Skladiste(2, "Zagreb", 200);
+            Skladiste n3 = new Skladiste(3, "Bjelovar", 200);
+            Proizvod p1 = new Proizvod(104, "Pivo", n1);
+            Proizvod p2 = new Proizvod(100, "Voda", n2);
+            Proizvod p3 = new Proizvod(10, "Kruh", n1);
+            Proizvod p4 = new Proizvod(70, "Limun", n2);
+            Proizvod p5 = new Proizvod(101234, "Boca", n1);
+            Proizvod p6 = new Proizvod(10110, "Laptop", n2);
 
+            List<Proizvod> proizvodi = new List<Proizvod>() { p1, p2, p3, p4, p5, p6 };
 
-            //query
-            var query = from grad in gradovi
-                        where grad.StartsWith("S")
-                        select grad;
+            var grupirano = proizvodi.GroupBy(p => p.Skladiste1).ToList();
 
-            // query.GetEnumerator();
-
-            //selektor
-            var kveri = gradovi
-                .Where(x => x.StartsWith("S"));
-
-            foreach (var grad in query)
+            foreach (var grupa in grupirano)
             {
-                Console.WriteLine(grad);
+                Console.WriteLine($"Skladiste: {grupa.Key}");
+                foreach (var proizvod in grupa)
+                {
+                    Console.WriteLine($"{proizvod.Name}");
+                }
             }
+            Console.ReadLine();
+        }
 
-            var artikli = new List<Artikl>()
+        private static void Drugi()
+        {
+            ArrayList arrayList = new ArrayList()
             {
-                new Artikl(104, "Pivo", 24),
-                new Artikl(100, "Voda", 124),
-                new Artikl(213, "Sok"),
-                new Artikl(974, "Kruh")
-
+                new Skladiste(1,"Zagreb", 300),
+                new Skladiste(2, "Zagreb", 200),
+                new Skladiste(3, "Bjelovar", 200),
+                new Proizvod(104, "Pivo", 24, "Pice"),
+                new Proizvod(100, "Voda", 124, "Pice"),
             };
 
+            var filtriranaSkladista = arrayList.OfType<Skladiste>().ToList();
+            foreach (var skl in filtriranaSkladista)
+            {
+                Console.WriteLine($"Lokacija: {skl.Lokacija}, Kapacitet: {skl.Kapacitet}");
+            }
 
 
-            artikli.ForEach(x => Console.WriteLine(x));
+            ///treci
+            var sortiraniUpit = filtriranaSkladista.OrderBy(skl => skl.Lokacija).ThenBy(n => n.Kapacitet).ToList();
+            Console.WriteLine("\nSORTIRANA SKLADISTA\n");
+            foreach (var skl in sortiraniUpit)
+            {
+                Console.WriteLine($"Lokacija: {skl.Lokacija}, Kapacitet: {skl.Kapacitet}");
+            }
 
-            var upit = (from artikl in artikli where artikl.Stanje > 0 select new { artikl.Sifra }).ToList();
-
-
-
-            var upit2 = artikli.Where(artikl => artikl.Stanje > 0).Select(y => new { y.Barkod, y.Sifra });
-
-
-            var sortirano = (from artikl in artikli orderby artikl.Name select artikl).ToList();
-
-
-            //komplikacije
-
-            var osnovni = from artikl in artikli
-                          where artikl.Stanje > 0
-                          select artikl;
-
-            var rezultirajuciUpit = osnovni.OrderBy(x => x.Name).Take(2);
+      
         }
+
+        private static void Prvi()
+        {
+            var proizvodi = new List<Proizvod>()
+            {
+                new Proizvod(104, "Pivo", 24, "Pice"),
+                new Proizvod(100, "Voda", 124, "Pice"),
+                new Proizvod(213, "Sok",32, "Pice"),
+                new Proizvod(974, "Kruh")
+
+            };
+            var upit = (from proizvod in proizvodi where proizvod.Stanje > 0 select new { proizvod.Sifra, proizvod.Name });
+
+            foreach (var proizvod in upit)
+            {
+                Console.WriteLine($"Ime: {proizvod.Name}, Sifra: {proizvod.Sifra}");
+            }
+
+            //treci zadatak
+            var sortiraniUpit = upit.OrderBy(p => p.Sifra).ThenBy(n => n.Name).ToList();
+            Console.WriteLine("\nSORTIRANI PROZIVODI\n");
+            foreach (var proizvod in sortiraniUpit)
+            {
+                Console.WriteLine($"Ime: {proizvod.Name}, Sifra: {proizvod.Sifra}");
+            }
+        }    
     }
 
-    public class Artikl
+    public class Proizvod
     {
-        public Artikl(int sifra, string naziv, int stanje =0)
+       
+        public Proizvod(int sifra, string naziv, int stanje = 0, string kategorija = "Hrana")
         {
-            Sifra = sifra;  
+            Sifra = sifra;
             Name = naziv;
             Stanje = stanje;
+            Kategorija = kategorija;
         }
+        public Proizvod(int sifra, string naziv, Skladiste skladiste, int stanje = 0, string kategorija = "Hrana")
+        {
+            Sifra = sifra;
+            Name = naziv;
+            Stanje = stanje;
+            Skladiste1 = skladiste;
+
+        }
+        
         public int Sifra { get; set; }
         public string Name { get; set; }
         public long Barkod { get; set; }
 
         public int Stanje { get; set; }
+        public string Kategorija { get; set; }
+        public Skladiste Skladiste1 { get; set; } 
+        public override string ToString()
+        {
+            return $"Sifra: {Sifra}, Name: {Name}, Skladiste: {Skladiste1.Lokacija}";
+        }
+    }
+    public class Skladiste
+    {
+        public int Id { get; set; }
+
+        public string Lokacija { get; set; }
+        public int Kapacitet { get; set; }
+
+        public Skladiste(int id, string lokacija, int kapacitet)
+        {
+            Id = id;
+            Lokacija = lokacija;
+            Kapacitet = kapacitet;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} Lokacija: {Lokacija}, Kapacitet: {Kapacitet}";
+        }
     }
 }
